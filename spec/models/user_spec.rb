955532d4be50db1f31570262a511b4395856e756
lcorr8 <l.corr.v@gmail.com>
2016-08-08 07:25:07 -0500

    stub out the first model test for users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+ 
+describe User do
+  it 'can be created' do
+    post = User.create!(email: "My email", password: "The encrypted password")
+    expect(post).to be_valid
+  end
+end
+
+hello?
+ 
