594d0a72c7b6668668be471d7f95884ade865611
lcorr8 <l.corr.v@gmail.com>
2016-09-12 11:38:09 -0400

    edit password digest column in user model

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20160830121155) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
