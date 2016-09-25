61f1ff803f6339e3a4875800fed18a2860f34a65
lcorr8 <l.corr.v@gmail.com>
2016-09-24 22:16:29 -0400

    stub out a home page to verify devise sign up and log in features

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable#, :omniauthable
 
 
 end
