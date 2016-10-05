3523a0a1c47fef96297b769a43630a57fba49492
lcorr8 <l.corr.v@gmail.com>
2016-10-05 19:33:23 -0400

    add simple validations to rating and video_rating models

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,7 @@
 class Rating < ActiveRecord::Base
   has_many :video_ratings
   has_many :videos, through: :video_ratings
+
+  validates :stars, presence: true
+  validates :stars, inclusion: 1..5
 end
--- app/models/video_rating.rb
+++ app/models/video_rating.rb
@@ -1,4 +1,8 @@
 class VideoRating < ActiveRecord::Base
   belongs_to :video 
   belongs_to :rating 
+
+  validates :reason, presence: true
+  validates :video_id, presence: true
+  validates :rating_id, presence: true
 end
