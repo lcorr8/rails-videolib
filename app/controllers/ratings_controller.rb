ae7ed8c52c946cc870ffbba79f599f885a2d6af3
lcorr8 <l.corr.v@gmail.com>
2016-10-05 23:30:21 -0400

    add before_action :set_video, edit and update action to rating controller; make a fields partial for new and edit video ratings; add edit link for rating on video show page

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,11 +1,12 @@
 class RatingsController < ApplicationController
+  before_action :set_video
+
   def new
-    @video = Video.find(params[:video_id])
     @rating = @video.video_ratings.build
   end
 
   def create
-    @video = Video.find(params[:video_id])
+    #raise params.inspect
     @rating = @video.video_ratings.build(rating_params)
     if @rating.save
       redirect_to video_path(@video)
@@ -15,7 +16,26 @@ class RatingsController < ApplicationController
     end
   end
 
+  def edit
+    @rating = VideoRating.find(params[:id])
+  end
+
+  def update
+    #raise params.inspect
+    @rating = VideoRating.find(params[:id])
+    if @rating.update(rating_params)
+      redirect_to video_path(@video)
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_video
+    @video = Video.find(params[:video_id])
+    #scope by user in the future
+  end
 
   def rating_params
     params.require(:video_rating).permit(:rating_id, :reason)
