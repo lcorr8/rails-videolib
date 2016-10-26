f400d67d9e0f6004fb791a1db14571dac9d15d6c
lcorr8 <l.corr.v@gmail.com>
2016-10-26 19:55:34 -0400

    adjust video show page, and add seed star ratings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@ Section.create(:id => 2, :name => "harry potter", :user_id => 1)
 Video.create(:id => 1, :name => "wig in a box", :link => "https://www.youtube.com/watch?v=E5V7_PSD4Sc",:year => "2015", :watched => "no", :embed_link => "", :section_id => 1, :user_id => 1)
 Video.create(:id => 2, :name => "Sugar Daddy", :link => "https://www.youtube.com/watch?v=uIaFn5lsLd8",:year => "2015", :watched => "no", :embed_link => "", :section_id => 1, :user_id => 1) 
 Video.create(:id => 3, :name => "fantastic beasts and where to find them", :link => "link", :year => "2015", :watched => "no", :embed_link => "", :section_id => 2, :user_id => 1)
+
+#test ratings, only 5 available. 1 = easy, 5 = hard
+Rating.create(:id => 1, :stars => 1)
+Rating.create(:id => 2, :stars => 2)
+Rating.create(:id => 3, :stars => 3)
+Rating.create(:id => 4, :stars => 4)
+Rating.create(:id => 5, :stars => 5)
