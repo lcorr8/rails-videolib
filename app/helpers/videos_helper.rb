1d8c9669d89a19a213f9da5cb6ab5034b2417d99
lcorr8 <l.corr.v@gmail.com>
2016-10-29 14:51:36 -0400

    edit video show helper to display embedded videos from aws properly

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,8 +1,12 @@
 module VideosHelper
 
-  def embed_html_from_youtube(youtube_url)
-    @youtube_video_id = youtube_url.split("=").last
-    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{@youtube_video_id}")
-    #<iframe width="560" height="315" src="https://www.youtube.com/embed/E5V7_PSD4Sc" frameborder="0" allowfullscreen></iframe>
+  def embed_html(url)
+    if url.include?("youtube")
+      @youtube_video_id = url.split("=").last
+      content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{@youtube_video_id}")
+    elsif url.include?("amazonaws")
+      #aws video just make an iframe with link as dynamic source
+      content_tag(:iframe, nil, src: "#{url}")
+    end
   end
 end
