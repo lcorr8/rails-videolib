f2f1b71a478fc3301d31adfcc3a326e4069ee7b1
lcorr8 <l.corr.v@gmail.com>
2016-10-30 15:17:59 -0400

    remove home action from applications controller, bc it is not being used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-  def home
-  end
-
   def after_sign_in_path_for(resource)
      sections_path
   end
-
 
   def study_suggestions
     @videos = Video.all
