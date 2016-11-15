8058516379df00cf12ec644ce9558f2b8c09408e
lcorr8 <l.corr.v@gmail.com>
2016-11-15 11:18:11 -0500

    update rating forms to display stars with description and a text area for the reason

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -8,6 +8,9 @@ class Rating < ActiveRecord::Base
   #1 = "so easy I can do it in my sleep"
   #2 = "oh this old thing?"
   #3 = "yeah I know how to do that"
-  #4 = "Theoretically i know how to do it, just have to look up some syntax"
+  #4 = "theoretically i know how to do it, just have to look up some syntax..."
   #5 = "gulp....help?"
+  def stars_with_description
+    "#{self.stars}: #{self.description}"
+  end
 end
