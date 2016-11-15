d36dd52bf59e9a363d1dbd3cc69513457dd4a8b2
lcorr8 <l.corr.v@gmail.com>
2016-11-15 00:04:24 -0500

    include pundit in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   include Pundit
+  #after_action :verify_authorized #ensure yu havent forgotten to authrize any actions
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def after_sign_in_path_for(resource)
     #devise helper.
@@ -12,6 +13,16 @@ class ApplicationController < ActionController::Base
 
   def study_suggestions
     @videos = Video.all
+  end
+
+  def home
+    skip_authorization
+  end
+
+  private
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || sections_path || root_path)
   end
   
 end
