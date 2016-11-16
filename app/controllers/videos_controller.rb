91f229e8520e33b3fb3838f0a2a3c4f8e90385a7
lcorr8 <l.corr.v@gmail.com>
2016-11-15 22:05:36 -0500

    display average video rating in video show action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,6 +6,8 @@ class VideosController < ApplicationController
     @video = Video.find(params[:id])
     authorize @video
     @user_ratings = @video.video_ratings.select{ |rating| rating.user_id == @user.id }
+    @average_rating = @video.video_ratings.average(:rating_id).to_i.to_s
+    @total_video_rating = Rating.find(@average_rating)
   end
 
   def new
