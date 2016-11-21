6e2e51e412f68bc97af5a67b12b46d225c931473
lcorr8 <l.corr.v@gmail.com>
2016-11-21 12:04:33 -0500

    stub out helpers for stats page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,33 @@ module ApplicationHelper
     @hard_ratings.to_a.uniq!{|rating| rating.video_id}
   end
 
+#OVERALL STATS:
+  #all flatiron videos
+  def flatiron_videos
+    @videos = Video.all 
+    @flatiron_videos = @videos.where(:flatiron => true)
+  end
+
+  #most watched videos (top 10 by everyone)
+
+  #hardest videos (top 10 videos with most 5 ratings)
+
+
+#PERSONAL STATS:
+
+  #watched flatiron videos by current user, for count.
+  def seen_flatiron_videos
+    WatchedVideo.where(:user_id => current_user, :flatiron => true)
+  end
+
+  #Videos you thought were difficult, to re-watch?
+
+  #videos you have yet to see?
+
+  #progress bar of % flatiron videos watched
+  #http://getbootstrap.com/components/#progress
+
+  #progress bar for % of total videos watched
+  #http://getbootstrap.com/components/#progress
+
 end
