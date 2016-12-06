eca6f239b90d1f149bbd85f37900221264a3278b
lcorr8 <l.corr.v@gmail.com>
2016-12-06 00:19:21 -0500

    note work on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include Pundit
-  #after_action :verify_authorized #ensure yu havent forgotten to authrize any actions
+  #after_action :verify_authorized #ensure you havent forgotten to authrize any actions
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  #devise helper
   def after_sign_in_path_for(resource)
-    #devise helper.
      sections_path
   end
 
@@ -16,10 +16,15 @@ class ApplicationController < ActionController::Base
   end
 
   def home
-    skip_authorization
+    #does devise give you a logged_in? helper? = user_igned_in? and current_user
+    if user_signed_in?
+      redirect_to sections_path
+    end
+      #skip_authorization
   end
 
   private
+  #helper written to rescue pundit policy errors with this message
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || sections_path || root_path)
