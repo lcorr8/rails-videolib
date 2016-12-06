94570e0f5a12adbfc5e97c153d362ae8bf0c038e
lcorr8 <l.corr.v@gmail.com>
2016-12-06 01:04:45 -0500

    note work on omniauth callbacks controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
  
+ #takes the auth hash from omniauth to create an user 
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user    
