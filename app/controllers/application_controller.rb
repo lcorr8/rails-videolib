39d914eefcde2c9955628ef03136b62f472c6ebf
lcorr8 <l.corr.v@gmail.com>
2016-12-12 13:26:12 -0500

    add instructions page, and modify navbar links to reflect these changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   #after_action :verify_authorized #ensure you havent forgotten to authrize any actions
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  before_action :authenticate_user!, only: [:study_suggestions]
 
   #devise helper
   def after_sign_in_path_for(resource)
@@ -21,16 +22,16 @@ class ApplicationController < ActionController::Base
 
   def home
     #does devise give you a logged_in? helper? => user_igned_in? and current_user
-    if user_signed_in?
-      redirect_to sections_path
-    end
+#    if user_signed_in?
+#      redirect_to sections_path
+#    end
       #skip_authorization
   end
 
   private
   #helper written to rescue pundit policy errors with this message
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:error] = "You are not authorized to perform this action."
     redirect_to(request.referrer || sections_path || root_path)
   end
   
