6b99ea91722cda86617b6893192c306f247a71f7
lcorr8 <l.corr.v@gmail.com>
2017-04-03 17:36:58 +0200

    stub out jquery notes to a video show page

---
+++ app/assets/javascripts/notes.js
@@ -0,0 +1,33 @@
+$(function(){
+  notesIndex();
+})
+
+function notesIndex(){
+  $(document).on("click", '#view-notes', function(e){
+    e.preventDefault()
+    var id = $(this).data("id")
+    //make requets to api
+    $.ajax({
+      method: "GET",
+      url: `/api/videos/${id}/notes`
+    }).success(function(data){
+      var notes = data
+      $(".video-notes").html('')
+      for (i=0; i< notes.length; i++) {
+        var note = new Note(notes[i].content)
+        var formattedNote = note.formatNote()
+        $(".video-notes").append(formattedNote)
+      }
+    })
+  })
+}
+
+function Note(content){
+  this.content = content
+}
+
+Note.prototype.formatNote = function() {
+  var html = ""
+  html += `<li>${this.content}</li>`
+  return html
+}
